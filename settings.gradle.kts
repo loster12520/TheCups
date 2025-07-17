f267ce854da7640f264179e7f3b2abd7ffa1dd14
lignting <peishuo_chen@geelark.com>
2025-07-17 17:13:04 +0800

    test

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+rootProject.name = "TheCups"
+
+pluginManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        mavenCentral()
+    }
+}
