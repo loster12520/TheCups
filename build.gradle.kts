d795f03c77f9efda3ead50054337a4e02bbeb87b
lignting <xiaoshuo12520@outlook.com>
2025-08-09 17:35:00 +0800

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,22 @@
+import kotlin.collections.map
+
 val TheCupVersion by extra { "0.0.1" }
 
 allprojects {
     group = "com.lignting"
     version = TheCupVersion
+}
+
+plugins {
+    id("idea")
+}
+
+idea {
+    module {
+        excludeDirs.addAll(
+            listOf(
+                "app/appMain/node_modules", "api/build", "buildSrc/build", ".gradle", "dist"
+            ).map { file(it) }
+        )
+    }
 }
