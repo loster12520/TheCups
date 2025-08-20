0db391adc6a4966fd7b013d814800ea994499af2
lignting <xiaoshuo12520@outlook.com>
2025-08-21 00:40:37 +0800

    20250821

--- api/src/main/kotlin/com/lignting/routes/Commands.kt
+++ api/src/main/kotlin/com/lignting/routes/Commands.kt
@@ -6,26 +6,9 @@ import io.ktor.server.routing.routing
 import io.ktor.server.websocket.receiveDeserialized
 import io.ktor.server.websocket.sendSerialized
 import io.ktor.server.websocket.webSocket
-import io.ktor.websocket.CloseReason
-import io.ktor.websocket.Frame
-import io.ktor.websocket.close
-import io.ktor.websocket.readText
-import io.ktor.websocket.send
 
 fun Application.commandsRouting() {
     routing {
-        webSocket("/echo") {
-            send("Please enter your name")
-            for (frame in incoming) {
-                frame as? Frame.Text ?: continue
-                val receivedText = frame.readText()
-                if (receivedText.equals("bye", ignoreCase = true)) {
-                    close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
-                } else {
-                    send(Frame.Text("Hi, $receivedText!"))
-                }
-            }
-        }
         webSocket("/command/test") {
             val input = receiveDeserialized<CommandWebSocketData>()
             when (input.type) {
--- api/src/main/kotlin/com/lignting/routes/Users.kt
+++ api/src/main/kotlin/com/lignting/routes/Users.kt
@@ -2,30 +2,25 @@ package com.lignting.routes
 
 import com.lignting.data.models.User
 import com.lignting.utils.JwtUtils
-import com.lignting.utils.call
 import io.ktor.server.application.Application
-import io.ktor.server.auth.authenticate
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
-import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 
 fun Application.usersRouting() {
     routing {
+        /**
+         * 用户登录
+         *
+         * 接收一个 User 对象，包含账号和密码，
+         * 返回一个 JWT 令牌.
+         */
         post("/users/login") {
             call.receive<User>().let {
                 JwtUtils.jwt(it.account)
             }.also {
                 call.respond(it)
-            }
-        }
-        
-        authenticate {
-            get("/test/1111") {
-                call {
-                    "OK"
-                }
             }
         }
     }
