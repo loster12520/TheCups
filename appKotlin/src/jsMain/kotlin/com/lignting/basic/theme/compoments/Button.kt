b97fd4ca5c14b2707e29bcba42d1888c10fe9f2d
xiaos <xiaoshuo12520@outlook.com>
2025-09-18 08:42:03 +0800

    20250918

--- appKotlin/src/jsMain/kotlin/com/lignting/basic/theme/compoments/Button.kt
+++ appKotlin/src/jsMain/kotlin/com/lignting/basic/theme/compoments/Button.kt
@@ -1,25 +1,30 @@
 package com.lignting.basic.theme.compoments
 
+import com.lignting.basic.encapsulation.style
 import com.lignting.basic.theme.Theme
 import emotion.react.css
 import react.FC
-import react.Key
 import react.Props
 import react.dom.html.ReactHTML.button
+import web.cssom.Border
 
-external class ButtonProps(
-    override var key: Key?,
-    val text: String,
-    val onClick: () -> Unit,
-) : Props
+external interface ButtonProps : Props {
+    var text: String
+    var onClick: () -> Unit
+}
 
 val Button = FC<ButtonProps> { props ->
     button {
-        css {
+//        css {
+//            color = Theme.background
+//            backgroundColor = Theme.primary
+//            border = "none".unsafeCast<Border>()
+//        }
+        style {
             color = Theme.background
             backgroundColor = Theme.primary
+            border = "none".unsafeCast<Border>()
         }
-        style
         key = props.key
         onClick = {
             props.onClick()
--- appKotlin/src/jsMain/kotlin/com/lignting/view/pages/Home.kt
+++ appKotlin/src/jsMain/kotlin/com/lignting/view/pages/Home.kt
@@ -1,10 +1,17 @@
 package com.lignting.view.pages
 
+import com.lignting.basic.theme.compoments.Button
 import react.FC
 import react.dom.html.ReactHTML.div
 
 val Home = FC {
     div {
-        +"Hello, Kotlin/JS with React!"
+        Button {
+            key = "button1"
+            text = "Click Me"
+            onClick = {
+                println("Button clicked!")
+            }
+        }
     }
 }
